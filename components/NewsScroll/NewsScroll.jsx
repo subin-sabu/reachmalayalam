60e2f673031b8ca1888f87150eff596b382ccb13
Subin Sabu <subincs2023@gmail.com>
2026-06-07 06:09:29 +0530

    Among the latest 5 news articles, only the ones with the tag 'scroll' will be shown in the NewsScroll

--- components/NewsScroll/NewsScroll.jsx
+++ components/NewsScroll/NewsScroll.jsx
@@ -1,28 +1,43 @@
-import React , { useContext }from 'react';
+import React, { useContext } from 'react';
 import styles from './NewsScroll.module.css';
 import { NewsContext } from '../../contexts/NewsContext';
-import  Box  from '@mui/material/Box';
+import Box from '@mui/material/Box';
 import { BulletContext } from '../../contexts/BulletContext'
 
 
+function dynamicIO(input, target) {
+  if (!input.length) return [];
+  if (target <= 0) return [];
+  // Make copies of array items to have target number of items finally.
+  return Array.from({ length: target }, (_, i) => input[i % input.length]);
+}
+ 
+
 function NewsScroll() {
-   
-  const {news, contextLoading } = useContext(NewsContext);
-  const scrollNews = news.slice(0,3)
+
+  const { news, contextLoading } = useContext(NewsContext);
+  const regex = /^\s*scroll\s*$/i; // Regular expression to match 'scroll' in a case-insensitive manner
+  const scrollNews = news.slice(0, 5).filter(item => Array.isArray(item.tags) && item.tags.some(tag => regex.test(tag)));
+  // Filering news items that have 'Scroll' tag and taking only the latest 3 news items. This is to ensure that the news scroll is not overloaded with too many items and also to maintain the relevance of the news being displayed in the scroll.
+
   
   const bulletsArray = useContext(BulletContext);
-  const scrollBullet = bulletsArray.slice(0,0)
+  const scrollBullet = bulletsArray.slice(0, 0);
 
-  const scrollArray = [...scrollNews, ...scrollBullet, ...scrollNews, ...scrollBullet, ...scrollNews, ...scrollBullet,...scrollNews, ...scrollBullet, ...scrollNews, ...scrollBullet, ...scrollNews, ...scrollBullet, ...scrollNews]
+  const sourceArray = [...scrollNews, ...scrollBullet];
+
+  const placeholder = [{ title: "For the latest news analysis and updates, follow us..." }, {title: "Send your articles to reachmalayalam@gmail.com"}];
+  
+  const scrollArray = contextLoading ? [] :dynamicIO(sourceArray.length ? sourceArray : placeholder, 28);
 
   return (
     <Box className={styles['scroll-container']}  >
       <div className={styles.scroll}>
-        
+
         {scrollArray.map((newsItem, index) => (
           <span key={index}>{newsItem.title}</span>
         ))}
-       
+
       </div>
     </Box>
   );
